9fffdeddab47e43f38ae73a68c8b17abfc168142
Tanim-Bhuiyann <tanimh64@gmail.com>
2024-10-14 20:04:28 +0600

    ok

--- http-file-server.js
+++ http-file-server.js
@@ -2,4 +2,6 @@ require('http').createServer((req, res) =>
     require('fs').createReadStream(process.argv[3])
         .pipe(res)
         .on('error', () => res.writeHead(500).end('Internal Server Error'))
+        .on('open', () => res.writeHead(200, { 'Content-Type': 'text/plain' }))
 ).listen(Number(process.argv[2]));
+
